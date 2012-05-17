27b9a1a8c09bcf9252c78c684e931b156906048c
Garrett Brown <garbearucla@gmail.com>
2012-05-16 23:43:45 -0700

    [avr] Added pt library and created a simple example program using pt.

--- avr/src/base.cpp
+++ avr/src/base.cpp
@@ -1,15 +1,140 @@
 #include <Arduino.h>
+#include <pt.h>
 
-const int pwm_pins[] = {4, // Green
-                        7, // Amber
-                        8, // Red
-                        9, // UV
-                        10}; // Emergency
-const int num_pwm = sizeof(pwm_pins) / sizeof(pwm_pins[0]);
-const int led_pins[] = {46, 44, 48, 42, 50, 52};
-const int num_leds = sizeof(led_pins) / sizeof(led_pins[0]);
+#include "hardware_interface.h"
 
-int current_led = led_pins[0];
+static struct pt pt1, pt2;
+
+static int protothread1(struct pt *pt);
+//static int protothread2(struct pt *pt);
+
+void setup()
+{
+	// Configure our pins
+	pinMode(LED_BATTERY_EMPTY, OUTPUT);
+	pinMode(LED_BATTERY_LOW, OUTPUT);
+	pinMode(LED_BATTERY_MEDIUM, OUTPUT);
+	pinMode(LED_BATTERY_HIGH, OUTPUT);
+
+	/*
+	digitalWrite(LED_BATTERY_EMPTY, LOW);
+	digitalWrite(LED_BATTERY_LOW, LOW);
+	digitalWrite(LED_BATTERY_MEDIUM, LOW);
+	digitalWrite(LED_BATTERY_HIGH, LOW);
+	*/
+
+	// Set up our protothreads
+	PT_INIT(&pt1);
+	//PT_INIT(&pt2);
+}
+
+void loop()
+{
+	protothread1(&pt1);
+	//protothread2(&pt2);
+}
+
+/*
+// protothread1 "local" variables
+static int batteryLedsOn[4];
+static int batteryLevel = 3; // High
+static int batteryState;
+static int batteryMillis;
+static int i;
+
+static int protothread1(struct pt *pt)
+{
+	PT_BEGIN(pt);
+	while(1)
+	{
+		// Calculate the battery state
+		for (batteryState = 0; batteryState < 4; ++batteryState)
+			if (!batteryLedsOn[batteryState])
+				break;
+
+		if (batteryState == batteryLevel)
+			batteryMillis = millis() + 2000;
+		else
+			batteryMillis = millis() + 200;
+
+		// Block until the amount of time has elapsed
+		PT_WAIT_UNTIL(pt, batteryMillis < millis());
+
+		for (i = 0; i < 4; ++i)
+		{
+			if (i <= batteryState)
+			{
+				// Turn on the
+			}
+			if (batteryLedsOn[i])
+		}
+		// Update our LEDs
+		if (batteryState == batteryLeds)
+		{
+			// batteryLevel might have changed, so disable all LEDs
+			digitalWrite(LED_BATTERY_LOW, LOW);
+			digitalWrite(LED_BATTERY_MEDIUM, LOW);
+			digitalWrite(LED_BATTERY_HIGH, LOW);
+		}
+		else if (batteryState == BatteryLow)
+		{
+			&
+		}
+	}
+	PT_END(pt);
+}
+*/
+
+static int ledMillis;
+static int ledState = 0;
+
+static int protothread1(struct pt *pt)
+{
+	PT_BEGIN(pt);
+	while(1)
+	{
+		ledMillis = millis() + 1000;
+
+		PT_WAIT_UNTIL(pt, ledMillis < millis());
+
+		//if (ledState == 1)
+		//	ledState = 0;
+		//else
+			ledState = 1;
+
+		digitalWrite(LED_BATTERY_EMPTY, ledState);
+		digitalWrite(LED_BATTERY_LOW, ledState);
+		digitalWrite(LED_BATTERY_MEDIUM, ledState);
+		digitalWrite(LED_BATTERY_HIGH, ledState);
+
+		PT_END(pt);
+	}
+}
+
+/*
+static int protothread2(struct pt *pt)
+{
+	PT_BEGIN(pt);
+	while(1)
+	{
+		PT_WAIT_UNTIL(pt, function_returns_true() );
+		do_something();
+	}
+	PT_END(pt);
+}
+
+/*
+static int protothread1(struct pt *pt) {
+    PT_BEGIN(pt);
+    while(1) {
+        PT_WAIT_UNTIL(pt, function_returns_true() );
+        do_something();
+    }
+    PT_END(pt);}
+*/
+
+
+/*
 
 void setup()
 {
@@ -73,8 +198,8 @@ void loop()
     }
     delay(2);
   }
-  */
+  /**
   increment_led();
   delay(200);
 }
-
+*/
---
+++ avr/src/hardware_interface.h
@@ -0,0 +1,14 @@
+
+#define LED_GREEN     4
+#define LED_YELLOW    7
+#define LED_RED       8
+#define LED_UV        9
+#define LED_EMERGENCY 10
+
+#define LED_STATUS_GREEN   44
+#define LED_STATUS_YELLOW  46
+#define LED_BATTERY_EMPTY  48
+#define LED_BATTERY_LOW    42
+#define LED_BATTERY_MEDIUM 50
+#define LED_BATTERY_HIGH   52
+
