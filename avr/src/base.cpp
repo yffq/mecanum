ac79071e2862a9d338b8b4c407483d2a1773a5d5
Garrett Brown <garbearucla@gmail.com>
2012-05-18 13:21:50 -0700

    [avr] Remove pt library - it had a memory leak or bug that froze the Arduino after 66 blinks (120 steps)

--- avr/src/base.cpp
+++ avr/src/base.cpp
@@ -3,29 +3,9 @@
 #include "hardware_interface.h"
 
 #include <Arduino.h>
-#include <pt.h>
 
-
-static FSMVector fsmv;
-static struct pt ptv[MAX_FSM];
-static int fsm_delay[MAX_FSM];
-
-/**
- * Entry point of each protothread. By treating each FSM as an array of data to
- * be operated upon, we give rise to a SIMD architecture and thus use a single
- * protothread function to process our numerous state machines.
- */
-static int protothread(struct pt *pt, int i)
-{
-	PT_BEGIN(pt);
-	while(1)
-	{
-		fsm_delay[i] = millis() + fsmv[i]->Delay();
-		PT_WAIT_UNTIL(pt, fsm_delay[i] <= millis());
-		fsmv[i]->Step();
-	}
-	PT_END(pt);
-}
+FSMVector fsmv;
+int fsm_delay[MAX_FSM];
 
 /**
  * Arduino provides the main() function; it looks like this:
@@ -36,19 +16,25 @@ static int protothread(struct pt *pt, int i)
  *         do_something_with_hw_serial();
  *     }
  * }
- * In setup(), we initialize our protothreads.
  */
 void setup()
 {
-	for (int i = 0; i < sizeof(ptv) / sizeof(ptv[0]); ++i)
-		PT_INIT(&ptv[i]);
-
 	// Test FSMs
 	fsmv.PushBack(new Blink(LED_BATTERY_EMPTY, 250));
+	fsmv.PushBack(new Blink(LED_BATTERY_LOW, 250));
+	fsmv.PushBack(new Blink(LED_BATTERY_MEDIUM, 250));
+	fsmv.PushBack(new Blink(LED_BATTERY_HIGH, 250));
 }
 
 void loop()
 {
 	for (int i = 0; i < fsmv.GetSize(); ++i)
-		protothread(&ptv[i], i);
+	{
+		// Wait until the delay has elapsed
+		if (fsm_delay[i] <= millis())
+		{
+			fsmv[i]->Step();
+			fsm_delay[i] = fsmv[i]->Delay() + millis();
+		}
+	}
 }
