5794a8a695adaae0ab4753ea763307387fbcb4df
Garrett Brown <garbearucla@gmail.com>
2012-06-04 00:49:56 -0700

    [arm] installRosMobile.py

--- arm/scripts/installRosMobile.py
+++ arm/scripts/installRosMobile.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 import os
+import fileinput
 import subprocess
+import urllib2
 
-from common import getScriptDir, ensureDependencies
+from common import getScriptDir
 
 # Configuration parameters
 distro='fuerte'
@@ -21,21 +23,75 @@ target='~/fuerte'
 def installMobile():
 	# Make sure our current working dir is the script's location
 	os.chdir(getScriptDir())
-	pass
+	
+	if not os.path.exists('/opt/ros/fuerte/setup.bash'):
+		subprocess.call(['sudo', './installRosCore.py'])
+		if not os.path.exists('/opt/ros/fuerte/setup.bash'):
+			print('Script failed: sudo ./installRosCore.py')
+			return
+	
+	# Acquire sudo privileges for later
+	subprocess.call(['sudo', 'cat', '/dev/null'])
+	
+	url = 'http://packages.ros.org/cgi-bin/gen_rosinstall.py?rosdistro=' + \
+			distro + '&variant=' + variant + '&overlay=no'
+	subprocess.call(['rosinstall', os.path.expanduser(target), url])
+	
+	# Environment setup
+	addBashSource('/opt/ros/fuerte/setup.bash')
+	# Update the environment so that the changes take effect immediately
+	if not os.environ.has_key('ROS_ROOT'):
+		updateEnvironment('/opt/ros/fuerte/setup.bash')
+	# Bug: Why does this get a newline after ~ ?
+	targetsrc = os.path.expanduser(target + '/setup.bash').replace('\n', '')
+	addBashSource(targetsrc)
+	if not os.environ.has_key('ROS_WORKSPACE'):
+		updateEnvironment(targetsrc)
+	
+	# Build higher-level libraries and tools
+	subprocess.call(['sudo', 'rosdep', 'init'])
+	subprocess.call(['sudo', 'rosdep', 'update']) # This might not need sudo
+	
+	# Install dependencies (from source, if necessary)
+	installDependencies()
+	
+	# Build the ROS stacks using rosmake
+	subprocess.call(['rosmake', '-a'])
 
-#'rosinstall ' + target + ' "http://packages.ros.org/cgi-bin/gen_rosinstall.py?"' +
-#	'rosdistro=' + distro + '&variant=' + variant + '&overlay=no"'
+def addBashSource(sourcetarget):
+	envstr = 'source ' + sourcetarget + '\n'
+	# Bug: sometimes, path get a newline after ~
+	bashrc = os.path.expanduser('~/.bashrc').replace('\n', '')
+	if not contains(bashrc, envstr):
+		file = open(bashrc, 'a')
+		file.write(envstr)
+		file.close()
+	return
 
-# Environment setup
-#'echo "source ' + target + '/setup.bash" > ~/.bashrc'
+# Remember, include '\n' in your searchExp
+def contains(filepath, searchExp):
+	for line in fileinput.input(filepath):
+		if line == searchExp:
+			fileinput.close()
+			return True
+	fileinput.close()
+	return False
 
-# Build higher-level libraries and tools
-#sudo rosdep init
-#rosdep update ### This might need sudo
-# Install system dependencies
-#rosdep install -a
-# Build the ROS stacks using rosmake
-#rosmake -a
+def updateEnvironment(source):
+	command = ['bash', '-c', 'source ' + source + ' && env']
+	proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+	for line in proc.stdout:
+		(key, _, value) = line.partition("=")
+		# Same bug as above: everything seems to get a \n appended to it
+		os.environ[key] = value.replace('\n', '')
+	proc.communicate()
+	#pprint.pprint(dict(os.environ))
+	return
+
+def installDependencies():
+	# Install system dependencies
+	subprocess.call(['rosdep', 'install', '-a']) # -ay
+
 
 if __name__ == '__main__':
 	installMobile()
