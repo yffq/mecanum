yffq/mecanum

commit 388c1415a2b2e139cf560b89c6d75399843f9ad0

[avr] Converted ChristmasTree and Fade over to new message-based instantiation method

avr/src/FiniteStateMachine.h
@@ -11,19 +11,8 @@
  */
 class FiniteStateMachine
 {
-protected:
-	/**
-	 *
-	 */
-	ByteArray parameters;
-
 public:
-	/**
-	 *
-	 */
-	FiniteStateMachine() { }
-
-	FiniteStateMachine(const ByteArray params) : parameters(params) { }
+	FiniteStateMachine *NewFromMsg(ByteArray msg);
 
 	/**
 	 * The destructor is declared virtual so that subclasses can optionally
@@ -36,14 +25,14 @@ public:
 	 */
 	unsigned char ID() const { return parameters[0]; }
 
+	const ByteArray &Describe() const { return parameters; }
+
 	/**
 	 * A FSM is uniquely identified by its parameters. A FSM should not alter
 	 * parameters after instantiation, because it would in effect transform
 	 * itself into a dissimilar FSM.
 	 */
 	bool operator==(const FiniteStateMachine &other) const { return parameters == other.parameters; }
-
-	const ByteArray &Describe() const { return parameters; }
 
 	/**
 	 * Take action and/or transition to the next state.
@@ -83,7 +72,16 @@ public:
 	 * deleted by the master. Also, don't use a message class, use a FSM's
 	 * SUBSCRIBER header.
 	 */
-	virtual bool Message(const ByteArray msg) { return false; }
+	virtual bool Message(const ByteArray &msg) { return false; }
+
+protected:
+	void DeclareParameters(unsigned char *params, unsigned char len) { parameters = ByteArray(params, len); }
+
+private:
+	/**
+	 *
+	 */
+	ByteArray parameters;
 };
 
 #endif // FINITESTATEMACHINE_H

avr/CMakeLists.txt
@@ -56,9 +56,9 @@ set(mecanum_srcs src/main.cpp
 #                 src/BatteryMonitor.cpp
                  src/Blink.cpp
                  src/ByteArray.cpp
-#                 src/ChristmasTree.cpp
+                 src/ChristmasTree.cpp
 #                 src/DigitalPublisher.cpp
-#                 src/Fade.cpp
+                 src/Fade.cpp
                  src/FSMVector.cpp
                  src/MecanumMaster.cpp
 #                 src/Mimic.cpp

avr/include/ByteUtils.h
@@ -1,38 +1,0 @@
-/*
- * ByteUtils.h
- *
- *  Created on: Jun 9, 2012
- *      Author: garrett
- */
-
-#ifndef BYTEUTILS_H_
-#define BYTEUTILS_H_
-
-namespace ByteUtils
-{
-	/**
-	 * Precondition: buffer's size is at least 4. To achieve compatibility with
-	 * 8-bit processors, only the lowest 4 bytes of n are serialized.
-	 * Postcondition: buffer contains n in big endian format.
-	 */
-	void Serialize(unsigned long n, unsigned char *buffer)
-	{
-		n &= 0xFFFFFFFF;
-		buffer[0] = n >> 24; // big endian
-		buffer[1] = n >> 16 & 0xFF;
-		buffer[2] = n >> 8 & 0xFF;
-		buffer[3] = n & 0xFF;
-	}
-
-	/**
-	 * The pre- and post-conditions are similar to above, but probably reversed.
-	 */
-	void Deserialize(const unsigned char *buffer, unsigned long &n)
-	{
-		n = static_cast<unsigned long>(buffer[0]) << 24 |
-		    static_cast<unsigned long>(buffer[1]) << 16 |
-		    static_cast<unsigned long>(buffer[2]) << 8 | buffer[3];
-	}
-}
-
-#endif // BYTEUTILS_H_

avr/src/Blink.cpp
@@ -1,7 +1,6 @@
 #include "Blink.h"
 
 #include "AddressBook.h"
-#include "ByteUtils.h"
 
 #include <Arduino.h>
 
@@ -11,36 +10,26 @@
 
 Blink::Blink(uint8_t pin, unsigned long delay) : m_delay(delay), m_enabled(false)
 {
+	//  Make the super class aware of our parameters
 	m_params[PARAM_ID] = FSM_BLINK;
 	m_params[PARAM_PIN] = pin;
-	ByteUtils::Serialize(delay, m_params + PARAM_DELAY);
+	ByteArray::Serialize(m_delay, m_params + PARAM_DELAY);
+	DeclareParameters(m_params, sizeof(m_params));
 
-	//  Make the super class aware of our params array
-	parameters = ByteArray(m_params, sizeof(m_params));
-
-	Init();
-}
-
-Blink::Blink(const ByteArray &params) : m_enabled(false)
-{
-	for (unsigned char i = 0; i < params.Length(); ++i)
-		m_params[i] = params[i];
-
-	// Why do we need a temporary variable here?
-	unsigned long n;
-	ByteUtils::Deserialize(m_params + PARAM_DELAY, n);
-	m_delay = n;
-
-	//  Make the super class aware of our params array
-	parameters = ByteArray(m_params, sizeof(m_params));
-
-	Init();
-}
-
-void Blink::Init()
-{
+	// Initialize the blinker
 	pinMode(m_params[PARAM_PIN], OUTPUT);
 	digitalWrite(m_params[PARAM_PIN], LOW);
+}
+
+Blink *Blink::NewFromArray(const ByteArray &params)
+{
+	if (params.Length() >= 6 && params[PARAM_ID] == FSM_BLINK)
+	{
+		unsigned long delay;
+		ByteArray::Deserialize(&params[PARAM_DELAY], delay);
+		return new Blink(params[PARAM_PIN], delay);
+	}
+	return 0;
 }
 
 Blink::~Blink()

avr/src/Blink.h
@@ -19,9 +19,11 @@ public:
 	 */
 	Blink(uint8_t pin, unsigned long delay /* ms */);
 
-	Blink(const ByteArray &params);
-
-	void Init();
+	/**
+	 * Performs parameter validation and instantiates a new object. If the
+	 * parameters are invalid or allocation fails, this function returns 0.
+	 */
+	static Blink *NewFromArray(const ByteArray &params);
 
 	/*
 	 * When this blinker is destructed, the pin is pulled low as a post-

avr/src/ByteArray.cpp
@@ -34,3 +34,18 @@ void ByteArray::Dump(unsigned char *buffer) const
 		buffer[i] = bytes[i - 1];
 	buffer[0] = length + 1;
 }
+
+void ByteArray::Serialize(unsigned long n, unsigned char *buffer)
+{
+	buffer[0] = n >> 24; // big endian
+	buffer[1] = n >> 16 & 0xFF;
+	buffer[2] = n >> 8 & 0xFF;
+	buffer[3] = n & 0xFF;
+}
+
+void ByteArray::Deserialize(const unsigned char *buffer, unsigned long &n)
+{
+	n = static_cast<unsigned long>(buffer[0]) << 24 |
+	    static_cast<unsigned long>(buffer[1]) << 16 |
+	    static_cast<unsigned long>(buffer[2]) << 8 | buffer[3];
+}

avr/src/ByteArray.h
@@ -48,6 +48,18 @@ public:
 	unsigned char Length() const { return length; }
 	void SetLength(unsigned char newLength) { length = newLength; }
 
+	/**
+	 * Precondition: buffer's size is at least 4. To achieve compatibility with
+	 * 8-bit processors, only the lowest 4 bytes of n are serialized.
+	 * Postcondition: buffer contains n in big endian format.
+	 */
+	static void Serialize(unsigned long n, unsigned char *buffer);
+
+	/**
+	 * The pre- and post-conditions are similar to above, but probably reversed.
+	 */
+	static void Deserialize(const unsigned char *buffer, unsigned long &n);
+
 private:
 	unsigned char *bytes;
 	unsigned char length;

avr/src/ChristmasTree.cpp
@@ -4,13 +4,23 @@
 
 #define SPEED 1200 // this gives a period of about 1285ms
 
-ChristmasTree::ChristmasTree() : FiniteStateMachine(FSM_CHRISTMASTREE), m_state(SpinningStart), m_spinningTarget(0), m_delay(50)
+ChristmasTree::ChristmasTree() : m_state(SpinningStart), m_spinningTarget(0), m_delay(50)
 {
+	m_params[0] = FSM_CHRISTMASTREE;
+	DeclareParameters(m_params, sizeof(m_params));
+
 	fader[0] = new Fade(LED_UV, SPEED, m_delay, Fade::LOGARITHMIC);
 	fader[1] = new Fade(LED_RED, SPEED, m_delay, Fade::LOGARITHMIC);
 	fader[2] = new Fade(LED_YELLOW, SPEED, m_delay, Fade::LOGARITHMIC);
 	fader[3] = new Fade(LED_GREEN, SPEED, m_delay, Fade::LOGARITHMIC);
 	fader[4] = new Fade(LED_EMERGENCY, SPEED, m_delay, Fade::LOGARITHMIC);
+}
+
+ChristmasTree *ChristmasTree::NewFromArray(const ByteArray &params)
+{
+	if (params.Length() >= 1 && params[0] == FSM_CHRISTMASTREE)
+		return new ChristmasTree();
+	return 0;
 }
 
 ChristmasTree::~ChristmasTree()

commit b6f6ef99ce928e8e1a7365f2b1167e21bb3067dc

[avr] Fix error introduced in 388c1415a2: Remove unused function FiniteStateMachine::NewFromMsg()

avr/src/FiniteStateMachine.h
@@ -12,8 +12,6 @@
 class FiniteStateMachine
 {
 public:
-	FiniteStateMachine *NewFromMsg(ByteArray msg);
-
 	/**
 	 * The destructor is declared virtual so that subclasses can optionally
 	 * override it to clean up their resources or fulfill postconditions.