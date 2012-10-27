9be7f82aac34b77fb6dcc9aa5549561248d88cb4
Garrett Brown <garbearucla@gmail.com>
2012-10-27 16:14:28 -0700

    Include avr package in Eclipse project generation (possibly broken)

--- make-eclipse-project.sh
+++ make-eclipse-project.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-# Run this in the stack directory to generate makefiles for the given packages
-# (space-separated). Make sure that the stack folder is included in
-# ROS_PACKAGE_PATH.
-rosmake --target=eclipse-project --specified-only avr_controller
+# Run this in the stack directory to generate makefiles for the packages listed
+# below. Make sure that this stack folder is included in ROS_PACKAGE_PATH.
+rosmake --target=eclipse-project --specified-only \
+	avr_controller
+
+# The avr package needs special treatment:
+cd avr
+cmake -G"Eclipse CDT4 - Unix Makefiles"
