b04d4deb42a08b7188c4dc1c8b1bb08c5f6ccec7
Garrett Brown <garbearucla@gmail.com>
2012-11-04 04:14:54 -0800

    [avr_controller] IMU: Experimental Gyro initialization code

--- avr_controller/src/IMU.cpp
+++ avr_controller/src/IMU.cpp
@@ -26,10 +26,10 @@
 #include <vector>
 #include <algorithm> // for std::find()
 
-#define I2C_ADDRESS_ADXL345   0x53
-#define I2C_ADDRESS_ITG3200   0x68
+#define I2C_ADDRESS_ADXL345       0x53
+#define I2C_ADDRESS_ITG3200       0x68
 
-// ADXL Register Map
+// ADXL345 Register Map
 #define	ADXL345_DEVID             0x00   // Device ID Register
 #define ADXL345_THRESH_TAP        0x1D   // Tap Threshold
 #define ADXL345_OFSX              0x1E   // X-axis offset
@@ -91,6 +91,51 @@
 #define ADXL345_SPI               (1<<6)
 #define ADXL345_SELF_TEST         (1<<7)
 
+// ITG3200 Register Map
+#define ITG3200_WHO_AM_I          0x00 // Who Am I
+#define ITG3200_SMPLRT_DIV        0x15 // Sample Rate Divider
+#define ITG3200_DLPF_FS           0x16 // DLPF, Full Scale
+#define ITG3200_INT_CFG           0x17 // Interrupt Configuration
+#define ITG3200_INT_STATUS        0x1A // Interrupt Status
+#define ITG3200_TEMP_OUT_H        0x1B // Temperature Data 0
+#define ITG3200_TEMP_OUT_L        0x1C // Temperature Data 1
+#define ITG3200_GYRO_XOUT_H       0x1D // Gyro-X Data 0
+#define ITG3200_GYRO_XOUT_L       0x1E // Gyro-X Data 1
+#define ITG3200_GYRO_YOUT_H       0x1F // Gyro-Y Data 0
+#define ITG3200_GYRO_YOUT_L       0x20 // Gyro-Y Data 1
+#define ITG3200_GYRO_ZOUT_H       0x21 // Gyro-Z Data 0
+#define ITG3200_GYRO_ZOUT_L       0x22 // Gyro-Z Data 1
+#define ITG3200_PWR_MGM           0x3E // Power Management
+
+// DLPF, Full Scale Register Bits
+// FS_SEL must be set to 3 for proper operation
+// Set DLPF_CFG to 3 for 1kHz Fint and 42 Hz Low Pass Filter
+#define ITG3200_DLPF_CFG_0        (1<<0)
+#define ITG3200_DLPF_CFG_1        (1<<1)
+#define ITG3200_DLPF_CFG_2        (1<<2)
+#define ITG3200_DLPF_FS_SEL_0     (1<<3)
+#define ITG3200_DLPF_FS_SEL_1     (1<<4)
+
+// Power Management Register Bits
+// Recommended to set CLK_SEL to 1,2 or 3 at startup for more stable clock
+#define ITG3200_PWR_MGM_CLK_SEL_0 (1<<0)
+#define ITG3200_PWR_MGM_CLK_SEL_1 (1<<1)
+#define ITG3200_PWR_MGM_CLK_SEL_2 (1<<2)
+#define ITG3200_PWR_MGM_STBY_Z    (1<<3)
+#define ITG3200_PWR_MGM_STBY_Y    (1<<4)
+#define ITG3200_PWR_MGM_STBY_X    (1<<5)
+#define ITG3200_PWR_MGM_SLEEP     (1<<6)
+#define ITG3200_PWR_MGM_H_RESET   (1<<7)
+
+// Interrupt Configuration Bits
+#define ITG3200_INT_CFG_ACTL         (1<<7)
+#define ITG3200_INT_CFG_OPEN         (1<<6)
+#define ITG3200_INT_CFG_LATCH_INT_EN (1<<5)
+#define ITG3200_INT_CFG_INT_ANYRD    (1<<4)
+#define ITG3200_INT_CFG_ITG_RDY_EN   (1<<2)
+#define ITG3200_INT_CFG_RAW_RDY_EN   (1<<0)
+
+
 using namespace std;
 
 bool IMU::Open()
@@ -104,6 +149,12 @@ bool IMU::Open()
 		{
 			if (InitAcc() && InitGyro())
 				return true;
+			else
+				cerr << "IMU::Open - Failed to initialize accelerometer and gyroscope settings" << endl;
+		}
+		else
+		{
+			cerr << "IMU::Open - Failed to detect devices 0x53 (ADXL345) and 0x68 (ITG3200)" << endl;
 		}
 	}
 	Close();
@@ -142,6 +193,23 @@ bool IMU::InitAcc()
 bool IMU::InitGyro()
 {
 	bool ret = true;
+
+	ret &= SetTarget(GYRO);
+
+	// Set internal clock to 1kHz with 42Hz LPF and Full Scale to 3 for proper operation
+	ret &= (i2c_smbus_write_byte_data(i2c.File(), ITG3200_DLPF_FS,
+			ITG3200_DLPF_FS_SEL_0 | ITG3200_DLPF_FS_SEL_1 | ITG3200_DLPF_CFG_0) >= 0);
+
+	// Set sample rate divider for 100 Hz operation (1KHz / (9 + 1))
+	ret &= (i2c_smbus_write_byte_data(i2c.File(), ITG3200_SMPLRT_DIV, 9) >= 0);
+
+	// Setup the interrupt to trigger when new data is ready
+	ret &= (i2c_smbus_write_byte_data(i2c.File(), ITG3200_INT_CFG,
+			ITG3200_INT_CFG_RAW_RDY_EN | ITG3200_INT_CFG_ITG_RDY_EN) >= 0);
+
+	// Select X gyro PLL for clock source
+	ret &= (i2c_smbus_write_byte_data(i2c.File(), ITG3200_PWR_MGM, ITG3200_PWR_MGM_CLK_SEL_0) >= 0);
+
 	return ret;
 }
 
