70c4b9d508fbeff8ff52f76c8ca789b1d5389036
Garrett Brown <garbearucla@gmail.com>
2012-11-25 00:38:09 -0800

    Project-generation: Init & update submodule if necessary

--- make-eclipse-project.sh
+++ make-eclipse-project.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+# First, make sure the arduino-cmake subrepository exists
+if [ ! -f avr/arduino-cmake/CMakeLists.txt ]
+then
+	git submodule init
+	git submodule update
+fi
+
 # Run this in the stack directory to generate makefiles for the packages listed
 # below. Make sure that this stack folder is included in ROS_PACKAGE_PATH.
 rosmake --target=eclipse-project --specified-only \
